2133374a3f315ce0764cd7518b12e34675b6ae2c
Da9TH5e <debarkomandal@gmail.com>
2025-03-20 15:28:03 +0530

    evaluation_phase

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig, TrainingConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig, EvaluationConfig,PrepareBaseModelConfig, TrainingConfig)
 
 
 class ConfigurationManager:
@@ -30,9 +30,6 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-    
-
-
 
 
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
@@ -74,3 +71,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/DATA",
+            mlflow_uri="https://dagshub.com/Da9TH5e/KidneyTumorCNN.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
